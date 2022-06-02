cba97b6c705c0267ec3d053c8a0684fccaa5a54e
renatomelo1 <94766906+renatomelo1@users.noreply.github.com>
2022-06-01 21:14:28 -0300

    Update ExecutarNoBancoDeDados.sql

--- ExecutarNoBancoDeDados.sql
+++ ExecutarNoBancoDeDados.sql
@@ -20,7 +20,7 @@ CREATE TABLE usuario (
 
 
 
-INSERT INTO genero(genro) VALUES ('Generico');
+INSERT INTO genero(genero) VALUES ('Generico');
 
 INSERT INTO filmes (nomefilme,posterfilme,codgenero,sinopsefilme) VALUES ('Cinema','https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-theater-poster-template-b7bbf00c3aab56f52e95d67fe65c655f_screen.jpg?ts=1636970736','1','Coming Soon')
 
